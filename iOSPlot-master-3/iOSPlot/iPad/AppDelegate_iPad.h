50e99f0aaf18bd29c757e297ec31b3489a0404d7
BSA Univ13 <bsauniv13@BSAs-iMac-2.local>
2014-06-23 19:18:38 +0530

    fisttt

---
+++ iOSPlot-master-3/iOSPlot/iPad/AppDelegate_iPad.h
@@ -0,0 +1,13 @@
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate_iPhone.h"
+
+@interface AppDelegate_iPad : AppDelegate_iPhone { //<UIApplicationDelegate> {
+    //UIWindow *window;
+}
+
+//@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ iOSPlot-master-3/iOSPlot/iPhone/AppDelegate_iPhone.h
@@ -0,0 +1,8 @@
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPhone : NSObject <UIApplicationDelegate>
+@property (nonatomic, strong) IBOutlet UIWindow *window;
+@end
+
